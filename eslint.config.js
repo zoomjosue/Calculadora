0281ab3afd031ff8e2d1ee7ec200004973575244
zoomjosue <jaigb@outlook.com>
2026-05-28 17:04:15 -0600

    Se configuro la base del proyecto

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      semi: ['error', 'never'],
+      'max-len': ['error', { code: 120 }],
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: { react: { version: 'detect' } },
+  },
+  { ignores: ['dist/**', 'node_modules/**', '.storybook/**'] },
+]
